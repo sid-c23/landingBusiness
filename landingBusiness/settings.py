5e20c434176e1b7edcf0f5d7b031d8a2515a424c
Sidhartha Chaganti <Sidhartha Chaganti>
2017-05-14 21:45:33 -0400

    another change

--- landingBusiness/settings.py
+++ landingBusiness/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*n06_t%ma(a-sss@&zj0^y(=jclng^*-z3%06uez-zywole(0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
