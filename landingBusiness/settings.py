24ad8d81010c5bcfc42b1dc78c256800efe21d8e
Sidhartha Chaganti <Sidhartha Chaganti>
2017-05-14 21:56:06 -0400

    another change

--- landingBusiness/settings.py
+++ landingBusiness/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*n06_t%ma(a-sss@&zj0^y(=jclng^*-z3%06uez-zywole(0='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['simplelanding.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['simplelanding.com', 'localhost', '127.0.0.1', 'simple-landing.herokuapp.com']
 
 
 # Application definition
