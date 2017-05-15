57fdd310a55f573a8e4e7951a39ddbeaef72e117
Sidhartha Chaganti <Sidhartha Chaganti>
2017-05-14 22:23:41 -0400

    another change

--- landingBusiness/settings.py
+++ landingBusiness/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*n06_t%ma(a-sss@&zj0^y(=jclng^*-z3%06uez-zywole(0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sic23.ddns.net', 'localhost', '127.0.0.1', 'simple-landing.herokuapp.com']
 
